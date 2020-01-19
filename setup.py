3d2ef166ca982a675839e05ec33137f6e1dce87d
Eli W. Hunter <elihunter173@gmail.com>
2020-01-18 19:44:52 -0500

    Follow recommendations of SO
    https://stackoverflow.com/questions/57915063/setup-py-setuptools-cmdclass-custom-commands-not-working

--- setup.py
+++ setup.py
@@ -1,47 +1,34 @@
-# Copyright (c) 2019, North Carolina State University Aerial Robotics Club
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the North Carolina State University Aerial Robotics Club
-#       nor the names of its contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from setuptools import Extension, setup
+# We import as _build_ext so that we can subclass it with our own
+from setuptools.command.build_ext import build_ext as _build_ext
 
-from distutils.core import setup, Extension
 
-import numpy as np
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
 
-tiffutils = Extension(
-    "tiffutils",
-    extra_compile_args=["-std=gnu99", "-g3"],
-    include_dirs=[np.get_include()],
-    library_dirs=["/usr/local/lib/"],
-    libraries=["tiff"],
-    sources=["tiffutils.c"],
-)
+        self.include_dirs.append(numpy.get_include())
+
 
 setup(
     name="tiffutils",
-    version="0.1.0",
+    version="1.0.0",
     description="Utilities for TIFF files",
     author="NC State Aerial Robotics Club",
     author_email="aerialrobotics@ncsu.edu",
     license="BSD",
-    ext_modules=[tiffutils],
+    # So that we can import numpy
+    cmdclass={"build_ext": build_ext},
+    setup_requires=["numpy"],
+    ext_modules=[
+        Extension(
+            "tiffutils",
+            extra_compile_args=["-std=gnu99", "-g3"],
+            libraries=["tiff"],
+            sources=["tiffutils.c"],
+        )
+    ],
 )
